91e51c3f11f231a805c90938dc929079cf37424e
ChristopherL <anbchris@gmail.com>
2025-08-06 22:26:10 -0400

    adding google fonts

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-14 bg-purple-951">
-      <h1 className="text-sky-600">Coffee Connoiseur</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between p-14">
+      <h1 className=" text-sky-600">Coffee Connoisseur</h1>
       <h4 className="text-yellow-400 text-9xl">I am a different color</h4>
     </main>
   );
