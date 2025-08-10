947ed2caf7306fc075127bd7bb64b5f64d85122b
ChristopherL <anbchris@gmail.com>
2025-08-09 23:51:39 -0400

    implement more routing

--- app/coffee-store/page.tsx
+++ app/coffee-store/page.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import Link from "next/link";
 
 export default function Page() {
-  return <div>Coffee Store Page</div>;
+  return (
+    <div>
+      <h1 className="pb-[30px]">Coffee Store Page</h1>
+      <button>
+        <Link href="/">Back to Home</Link>
+      </button>
+    </div>
+  );
 }
